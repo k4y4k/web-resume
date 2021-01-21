a4b1070cf4efff973dbcfda26bc61e03bea62b32
kayak <kayak@kayak.rocks>
2021-01-21 10:46:51 +1000

    ✅ jest + react testing library works
    added a dummy test, will replace asap

---
+++ jest-preprocess.js
@@ -0,0 +1,11 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+const babelOptions = {
+  presets: [
+    '@babel/preset-react',
+    'babel-preset-gatsby',
+    '@babel/preset-typescript',
+  ],
+}
+
+module.exports = require('babel-jest').createTransformer(babelOptions)

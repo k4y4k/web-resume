47e482adf32bf2d6f41eababbb5b954578bf543a
kayak <kayak@kayak.rocks>
2021-01-25 12:49:04 +1000

    ⚗ (storybook): reduce chunk size; may avoid OOM errors?

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,19 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  addons: [
+    // ...
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ]
+  webpackFinal: async (config, { configType }) => {
+    // storybookjs/storybook#6408 has this fix
+    config.optimization = {
+      splitChunks: {
+        chunks: 'all',
+        minSize: 30 * 1024, // 30KB
+        maxSize: 1024 * 1024, // 1MB
+      },
+    }
+
+    return config
+  },
 }

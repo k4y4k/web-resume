055846bfc010417341db2f568095e7acfe0c2f2c
kayak <kayak@kayak.rocks>
2021-01-28 15:46:32 +1000

    ✨ (section): item details
    powered by markdown 😄
    
    closes #57

---
+++ src/components/section/ItemDetails.test.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react'
+import { screen, render } from '@testing-library/react'
+import ItemDetails from './ItemDetails'
+
+describe('<ItemDetails />', () => {
+  test('no data', () => {
+    render(<ItemDetails />)
+
+    const details = screen.getByTestId('sectionItemDetails')
+    expect(details).toHaveTextContent('this section intentionally left blank')
+  })
+
+  test('renders markdown', () => {
+    const string = `
+- a
+- b
+- c
+`
+
+    render(<ItemDetails details={string} />)
+
+    const details = screen.getByTestId('sectionItemDetails')
+    expect(details).toBeInTheDocument()
+
+    expect(details).toHaveTextContent('abc')
+  })
+})

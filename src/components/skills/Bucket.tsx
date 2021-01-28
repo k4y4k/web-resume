9cc1080c8b048908f21e552360f9c15ab7ce90ad
kayak <kayak@kayak.rocks>
2021-01-29 00:02:34 +1000

    ♻️ (bucket): simplify conditional; use localeCompare for sort

--- src/components/skills/Bucket.tsx
+++ src/components/skills/Bucket.tsx
@@ -5,10 +5,9 @@ interface BucketTypes {
 }
 
 const Bucket = ({ skills }: BucketTypes): JSX.Element => {
-  if (skills === undefined ?? skills?.length === 0)
-    return <div data-testid='skillsBucket'></div>
+  if (skills === undefined) return <div data-testid='skillsBucket'></div>
 
-  const res = skills?.sort().join(' ')
+  const res = skills.sort((a, b) => a.localeCompare(b)).join(' ')
 
   return <p data-testid='skillsBucket'>{res}</p>
 }

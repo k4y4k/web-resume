a5ed9559ef8d5e3440a9f8aa3e79487af09d2e24
kayak <kayak@kayak.rocks>
2021-02-04 13:43:49 +1000

    💄 (index): more hover effects

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -29,7 +29,7 @@ export const ContactStyles = css`
 
   a,
   p {
-    ${tw`p-4 hover:bg-deeporange`}
+    ${tw`p-4 hover:bg-deeporange transition-all`}
   }
 `
 
--- src/components/section/SectionContainer.tsx
+++ src/components/section/SectionContainer.tsx
@@ -8,7 +8,17 @@ interface SectionContainerTypes {
 }
 
 const sectionContainerStyles = (): Array<TwStyle | string> => [
-  tw`m-4 border border-black`,
+  tw`m-4 border border-black transition-all	`,
+  tw`hover:bg-deeporange hover:bg-opacity-50 `,
+  `&:hover {
+	box-shadow:
+	  0 2.8px 2.2px rgba(181,61,39, 0.07),
+	  0 6.7px 5.3px rgba(181,61,39, 0.05),
+	  0 12.5px 10px rgba(181,61,39, 0.042),
+	  0 22.3px 17.9px rgba(181,61,39, 0.035),
+	  0 41.8px 33.4px rgba(181,61,39, 0.028),
+	  0 100px 80px rgba(181,61,39, 0.02);
+	}`,
 ]
 
 const SectionContainer = ({

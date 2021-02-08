b9bcc26b63aa74440246ff4b55b8c72da2e8bce8
kayak <kayak@kayak.rocks>
2021-02-08 20:16:56 +1000

    ✅ (getNetworkUsernames): add tests

---
+++ src/helpers/getNetworkUsernames.test.ts
@@ -0,0 +1,28 @@
+import faker from 'faker'
+import getNetworkUsernames from './getNetworkUsernames'
+
+describe('getNetworkUsernames', () => {
+  test('finds network in list, returns matching username', () => {
+    const randomUsername = faker.internet.userName()
+    const data = [
+      { network: 'spotify', username: faker.internet.userName() },
+      { network: 'twitter', username: randomUsername },
+      { network: 'baidu', username: faker.internet.userName() },
+    ]
+
+    expect(getNetworkUsernames(data, 'twitter')).toBe(randomUsername)
+  })
+
+  test('returns undefined if network not in list', () => {
+    const randomUsername = faker.internet.userName()
+    const data = [
+      {
+        network: 'foo',
+        // different username
+        username: faker.internet.userName(),
+      },
+    ]
+
+    expect(getNetworkUsernames(data, randomUsername)).toBeUndefined()
+  })
+})
--- src/helpers/getNetworkUsernames.ts
+++ src/helpers/getNetworkUsernames.ts
@@ -1,9 +1,14 @@
-// const twitter = getNetworkUsernames(profiles, 'twitter')
+/* This function accepts an array of profileItems and a string.
+ * It searches for a profileItem with a network property that matches the string (case insensitive)
+ * It returns the username inside the profileItem.
+ *
+ * eg for data = [{network:"a", username:"AA"}, {network:"b", username:"BB"}]
+ * calling getNetworkUsernames(data, "a") will return "AA"
+ */
 
 interface profileItem {
   network: string
   username: string
-  url?: string
 }
 
 const getNetworkUsernames = (
@@ -12,7 +17,8 @@ const getNetworkUsernames = (
 ): string => {
   const regex = new RegExp(networkName, 'i')
 
-  const res = profiles.filter((el: profileItem) => regex.test(el.network)) ?? ''
+  const res = profiles.filter((el: profileItem) => regex.test(el.network))
+
   return res[0]?.username
 }
 

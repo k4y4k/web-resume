682b538ca41259ef5f502b5e9424e8d2225b31d8
kayak <kayak@kayak.rocks>
2021-02-09 15:15:41 +1000

    🔇 (ItemContainer): remove log statement

--- src/components/section/ItemContainer.test.tsx
+++ src/components/section/ItemContainer.test.tsx
@@ -42,8 +42,6 @@ describe('<ItemContainer />', () => {
       />
     )
 
-    screen.debug()
-
     const title = screen.getByText('Bachelor of Software Development')
     expect(title).toBeInTheDocument()
 

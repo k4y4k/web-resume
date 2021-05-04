b1252ff06386fa61a1e95e64a3a6e2456dd548f9
kayak <kayak@kayak.rocks>
2021-05-04 23:42:40 +1000

    💄 (skills): remove forced sorting

--- src/components/skills/Bucket.tsx
+++ src/components/skills/Bucket.tsx
@@ -7,11 +7,9 @@ interface BucketTypes {
 const Bucket = ({ skills }: BucketTypes): JSX.Element | null => {
   if (skills.length === 1 && skills[0] === '') return null
 
-  const res = skills.sort((a, b) => a.localeCompare(b))
-
   return (
     <div data-testid='skillsBucket'>
-      {res.map(el => (
+      {skills.map(el => (
         <span key={el}>{`${el} `}</span>
       ))}
     </div>

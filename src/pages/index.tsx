69235628405e050fa8eae8e5f980a38e5c0d0ae9
kayak <kayak@kayak.rocks>
2021-06-11 15:25:20 +1000

    ✨ (index): get tab title from name
    closes #260

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,14 +9,27 @@ import Modal from '../components/modal/Modal'
 import '../index.css'
 import Resume from '../components/pages/Resume'
 import CoverLetter from '../components/pages/CoverLetter'
+import { graphql, useStaticQuery } from 'gatsby'
 
 const IndexRoute = (): JSX.Element => {
+  const { name }: { name: string } = useStaticQuery(graphql`
+    {
+      jsonName: file(extension: { eq: "json" }, name: { eq: "data" }) {
+        childDataJson {
+          basics {
+            name
+          }
+        }
+      }
+    }
+  `).jsonName.childDataJson.basics
+
   return (
     <IconContext.Provider value={{ className: 'icon' }}>
       <Helmet defer={false}>
         <html lang='en' />
         <meta charSet='utf-8' />
-        <title>{'Example Resumé | k4y4k/web-resume'}</title>
+        <title>{`${name} | k4y4k/web-resume`}</title>
         <meta name='description' content='Online resume.' />
         <link rel='canonical' href='https://kayak.rocks/web-resume' />
         <meta property='og:type' content='website' />

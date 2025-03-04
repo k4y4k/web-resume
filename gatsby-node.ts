87c62a81b23e94ae008f22e5b04a76c91cf8f62e
kayak <kayak@kayak.rocks>
2025-03-04 10:24:35 +1000

    bug(graphql): stop yelling at me for an optional boolean

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -3,12 +3,8 @@ import type { GatsbyNode } from "gatsby";
 export const createSchemaCustomization: GatsbyNode["createSchemaCustomization"] =
   ({ actions }) => {
     actions.createTypes(`
-    type Site {
-      siteMetadata: SiteMetadata!
-    }
-
-    type SiteMetadata {
-      title: String!
-    }
+      type DataJsonBasics implements Node {
+        technical: Boolean
+      }
   `);
   };
